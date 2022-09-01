e70e8fa0f7c2a7fcb1847b6c4900f550c1224782
Cagatay Kaydir <cagataykaydir@gmail.com>
2022-09-01 00:43:40 +0000

    coc bugfix

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 require("default")
-vim.cmd("so ~/.config/nvim/coc.vim")
+-- vim.cmd("so ~/.config/nvim/coc.vim")
+vim.cmd([[
+inoremap <silent><expr> <tab> coc#pum#visible() ? coc#pum#confirm()
+                              \: "\<C-g>u\<tab><c-r>=coc#on_enter()\<CR>"
+inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+]])
