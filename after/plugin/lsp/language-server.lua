c359677569e6d9eb21021409fe23f11c518fa1c5
Cagatay Kaydir <cagataykaydir@gmail.com>
2023-02-04 02:47:26 +0300

    minor changes

--- after/plugin/lsp/language-server.lua
+++ after/plugin/lsp/language-server.lua
@@ -2,6 +2,8 @@
 
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lsp_attach = function(client, bufnr)
+  local opts = { buffer = bufnr, remap = false }
+
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<C-n>", function() vim.diagnostic.goto_next() end, opts)
